823ac0df674a7c49c09a9490a8884aa1b8cbe713
[Tuba Asif] <[tubaasif146@gmail.com]>
2024-04-01 04:42:17 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+//Functions
+function greet(name) {
+    return "welcome, ".concat(name);
+}
+console.log(greet("Tuba Asif")); //welcome, Tuba Asif
+console.log(greet("Tuba")); // welcome,Tuba
+//another example
+function add(a, b) {
+    return a + b;
+}
+console.log((5 + 2)); //7
+console.log((5 - 2));
---
+++ main.ts
@@ -0,0 +1,15 @@
+//Functions
+
+function greet(name:string) {
+    return `welcome, ${name}`
+}
+console.log(greet("Tuba Asif")) //welcome, Tuba Asif
+console.log(greet("Tuba")) // welcome,Tuba
+
+
+//another example
+function add (a:number , b:number){
+    return a+b
+}
+console.log((5+2)) //7
+console.log ((5-2)) //3
